a1a4722c6c00468d67b2647d5d31eb94914cf0aa
Mateo <mateo.valencia.g@gmail.com>
2021-04-18 14:59:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lena/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.lena;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+                Intent i = new Intent(this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
