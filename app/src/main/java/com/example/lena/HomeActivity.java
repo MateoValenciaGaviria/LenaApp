43a23713de992a6d164f3175687818e99f613a9b
Mateo <mateo.valencia.g@gmail.com>
2021-04-19 05:37:13 -0500

    bar nav changed

--- app/src/main/java/com/example/lena/HomeActivity.java
+++ app/src/main/java/com/example/lena/HomeActivity.java
@@ -5,62 +5,20 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
-
 public class HomeActivity extends AppCompatActivity {
 
-    MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-
-        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic_testhome));
-        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_stadistics));
-        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_plant));
-        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.ic_person));
-
-        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
-            @Override
-            public void onShowItem(MeowBottomNavigation.Model item) {
-                Fragment fragment = null;
-
-                switch (item.getId()){
-                    case 1:
-                        fragment = new HomeFragment();
-                        break;
-                    case 2:
-                        fragment = new StatisticsFragment();
-                        break;
-                    case 3:
-                        fragment = new PlantsCareFragment();
-                        break;
-                    case 4:
-                        fragment = new ProfileFragment();
-                        break;
-                }
-                loadFragment(fragment);
-            }
-        });
-
-        //bottomNavigation.setCount(1, "10");
-        bottomNavigation.show(1, true);
-        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
-            @Override
-            public void onClickItem(MeowBottomNavigation.Model item) {
-
-            }
-        });
-
     }
 
     private void loadFragment(Fragment fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.frame_layout,fragment)
+                .replace(R.id.main_fragment,fragment)
                 .commit();
     }
 }
