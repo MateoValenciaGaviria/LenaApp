87f663384d8d63e0c029ca136b22d963a280e5ff
Mateo <mateo.valencia.g@gmail.com>
2021-04-19 17:02:44 -0500

    Home fragment started and search input added

--- app/src/main/java/com/example/lena/HomeActivity.java
+++ app/src/main/java/com/example/lena/HomeActivity.java
@@ -2,10 +2,20 @@ package com.example.lena;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.tbuonomo.morphbottomnavigation.MorphBottomNavigationView;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private HomeFragment homeFragment;
+    private StatisticsFragment statisticsFragment;
+    private PlantsCareFragment plantsCareFragment;
+    private ProfileFragment profileFragment;
+    private MorphBottomNavigationView navigator;
 
 
     @Override
@@ -13,12 +23,41 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        navigator = findViewById(R.id.navigator);
+
+        homeFragment = HomeFragment.newInstance();
+        statisticsFragment = StatisticsFragment.newInstance();
+        plantsCareFragment = PlantsCareFragment.newInstance();
+        profileFragment = ProfileFragment.newInstance();
+
+        showFragment(homeFragment);
+
+        navigator.setOnNavigationItemSelectedListener(
+                (menuItem) -> {
+                    switch (menuItem.getItemId()){
+                        case R.id.home:
+                            showFragment(homeFragment);
+                            break;
+                        case R.id.statistics:
+                            showFragment(statisticsFragment);
+                            break;
+                        case R.id.plants_care:
+                            showFragment(plantsCareFragment);
+                            break;
+                        case R.id.profile:
+                            showFragment(profileFragment);
+                            break;
+                    }
+
+                    return true; 
+                }
+        );
     }
 
-    private void loadFragment(Fragment fragment) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.main_fragment,fragment)
-                .commit();
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_fragment, fragment);
+        transaction.commit();
     }
 }
