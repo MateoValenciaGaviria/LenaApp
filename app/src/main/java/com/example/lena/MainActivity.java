0a32ee9440fe325a319bdbbb4b34896446881e4a
Mateo <mateo.valencia.g@gmail.com>
2021-04-21 03:19:20 -0500

    Dark mode disabled

--- app/src/main/java/com/example/lena/MainActivity.java
+++ app/src/main/java/com/example/lena/MainActivity.java
@@ -7,6 +7,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
