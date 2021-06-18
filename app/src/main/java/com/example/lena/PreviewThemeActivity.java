db6ae709256df315487b8b77d8fe1c4e98ce5ca3
Mateo <mateo.valencia.g@gmail.com>
2021-06-18 13:52:20 -0500

    Theme edit, preview activity added and main logo changed

---
+++ app/src/main/java/com/example/lena/PreviewThemeActivity.java
@@ -0,0 +1,14 @@
+package com.example.lena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreviewThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_theme);
+    }
+}
